20509c0285ffd668205a4d7505756f6b81a15902
Jen Caza <jencaza@gmail.com>
2022-01-31 21:14:01 -0500

    finalize plan

--- planning.rb
+++ planning.rb
@@ -1,15 +1,16 @@
 ## extract nouns for classes:
   # game > houses all the main code; gives structure to the game itself (behaviour)
-    # score - turn - winner
+  # initialize player1, player2, current_player
+    # METHODS: score - turn - start_game - winner
     # contains game loop
-    
-  # player > keeps players organized with # of lives they have (state)
-    # name - lives 
     # manages current player
+    # contains user I/O
+    
+  # player > initializes players by name and lives (state)
+    # initialize name, lives 
 
   # question > formatting the question and setting up how the answer is calculated (state)
-    # new_question,  sum
-      # initialize number1 and number2 for the question
-
-
-# user I/O?
+    # initialize number1, number2
+    # METHODS: new_question,  sum
+      # new_question template
+      # dictate what answer should be with sum

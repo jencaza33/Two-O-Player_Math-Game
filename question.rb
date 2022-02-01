157735e5a9dce0a6e87d8bc6621287662df35bc8
Jen Caza <jencaza@gmail.com>
2022-01-31 21:24:20 -0500

    set up game and questions

--- question.rb
+++ question.rb
@@ -1,16 +1,18 @@
 class Question
-    
+
     def initialize
-        @num1 = 
-        @num2 = 
+      @num1 = rand(1..20)
+      @num2 = rand(1..20)
     
-      end
-    
-      def new_question
-    
-      end
-    
-      def sum
-        @num1 + @num2
-      end
-end
+    end
+  
+    def new_question(name)
+      puts "#{name}: What does #{num1} plus #{num2} equal?"
+    end
+  
+    def sum
+      @num1 + @num2
+    end
+  
+  
+  end

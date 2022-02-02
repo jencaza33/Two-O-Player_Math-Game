20b13117b44c0eba7dff49d2b1f57a172d575ea8
Jen Caza <jencaza@gmail.com>
2022-02-02 15:39:17 -0500

    update question class and player class

--- question.rb
+++ question.rb
@@ -1,20 +1,10 @@
-require 'pp';
-
 class Question
-
-  def initialize
-    @num1 = rand(1..20)
-    @num2 = rand(1..20)
+    attr_accessor :num1, :num2
   
+    def initialize
+      @num1 = rand(1..20)
+      @num2 = rand(1..20)
+      @solution = @num1 + @num2
+    
+    end
   end
-
-  def new_question(name)
-    puts "#{name}: What does #{num1} plus #{num2} equal?"
-  end
-
-  def sum
-    @num1 + @num2
-  end
-
-
-end

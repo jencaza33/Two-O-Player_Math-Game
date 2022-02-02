d318c06b74037c9a90811113749fd070f86809df
Jen Caza <jencaza@gmail.com>
2022-02-02 15:25:40 -0500

    add title to command line

--- question.rb
+++ question.rb
@@ -1,18 +1,20 @@
+require 'pp';
+
 class Question
 
-    def initialize
-      @num1 = rand(1..20)
-      @num2 = rand(1..20)
-    
-    end
-  
-    def new_question(name)
-      puts "#{name}: What does #{num1} plus #{num2} equal?"
-    end
-  
-    def sum
-      @num1 + @num2
-    end
-  
+  def initialize
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
   
   end
+
+  def new_question(name)
+    puts "#{name}: What does #{num1} plus #{num2} equal?"
+  end
+
+  def sum
+    @num1 + @num2
+  end
+
+
+end

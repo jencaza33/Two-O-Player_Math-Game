344331f28de378b892590df6dacf30c5005f34bd
Jen Caza <jencaza@gmail.com>
2022-02-02 15:23:48 -0500

    add initialize for game class, add title to command line

--- game.rb
+++ game.rb
@@ -1,25 +1,28 @@
-require './players'
-require './question'
+require "./player"
+require "./question"
 
 class Game
-    def initialize
-      @player1
-      @player2
-      @current_player
-    end
-  
-  
-    def turn
-    end
-  
-    def start_game
-    end
-  
-  
-    def score
-    end
-  
-  
-  
-  
+  def initialize
+    @player1 = Player.new('Jen', 3)
+    @player2 = Player.new('Becky', 3)
+    @current_player = @player1
   end
+
+
+  def turn
+  end
+
+  def start_game
+    puts "----- TwO-O-Player -----"
+    puts "\n"
+    
+  end
+
+
+  def score
+  end
+
+
+
+
+end
